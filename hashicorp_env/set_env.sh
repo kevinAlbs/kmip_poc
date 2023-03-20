7a0abef65f61ace7b12518f52ce4b436a654e2c1
Kevin Albertson <kevin.albertson@mongodb.com>
2023-03-20 12:38:28 -0400

    add VAULT_BIN paths for 1.11.8, 1.13.0, and 1.13.1-dev

--- hashicorp_env/set_env.sh
+++ hashicorp_env/set_env.sh
@@ -2,5 +2,7 @@
 export VAULT_ADDR='http://127.0.0.1:8200'
 # Enterprise license was obtained during investigation of MONGOCRYPT-563.
 export VAULT_LICENSE=$(cat ~/.secrets/hashicorp-vault/enterprise-license.txt)
-export VAULT_BIN=~/bin/hashicorp_vault/vault_1.11.8+ent_darwin_arm64/vault
+# export VAULT_BIN=~/bin/hashicorp_vault/vault_1.11.8+ent_darwin_arm64/vault
+# export VAULT_BIN=~/bin/hashicorp_vault/vault_1.13.0+ent_darwin_arm64/vault
+export VAULT_BIN=~/bin/hashicorp_vault/vault-1.13.1-dev-darwin-arm64
 echo "Using VAULT_BIN=$VAULT_BIN"
