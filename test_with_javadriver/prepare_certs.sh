dcec1745f3a6ca4390b8fcb78c9cbb0033ac83ef
Kevin Albertson <kevin.albertson@mongodb.com>
2023-04-14 16:18:19 -0400

    add prepare_certs.sh

---
+++ test_with_javadriver/prepare_certs.sh
@@ -0,0 +1,15 @@
+# Prepare certificates for Java driver to authenticate to Hashicorp Vault server over TLS.
+# Copied and modified from: https://github.com/mongodb/mongo-java-driver/blob/master/.evergreen/run-csfle-tests-with-mongocryptd.sh
+# After running, add these Java system properties to IntelliJ:
+# -Djavax.net.ssl.enabled=true
+# -Djavax.net.ssl.keyStoreType=pkcs12
+# -Djavax.net.ssl.keyStore=/Users/kevin.albertson/code/kmip_poc/client.pkc
+# -Djavax.net.ssl.keyStorePassword=bithere
+# -Djavax.net.ssl.trustStoreType=jks
+# -Djavax.net.ssl.trustStore=/Users/kevin.albertson/code/kmip_poc/mongo-truststore
+# -Djavax.net.ssl.trustStorePassword=changeit
+CERT_PATH=/Users/kevin.albertson/.secrets/hashicorp-vault/kmip_certs
+KMIP_TLS_CERTIFICATE_KEY_FILE=$CERT_PATH/key_with_cert.pem
+KMIP_TLS_CA_FILE=$CERT_PATH/ca.pem
+openssl pkcs12 -CAfile $KMIP_TLS_CA_FILE -export -in $KMIP_TLS_CERTIFICATE_KEY_FILE -out client.pkc -password pass:bithere
+keytool -importcert -trustcacerts -file $KMIP_TLS_CA_FILE -keystore mongo-truststore -storepass changeit -storetype JKS -noprompt
