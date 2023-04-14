5aa5e8f9f1326402170ee8066e501aa5cfec2429
Kevin Albertson <kevin.albertson@mongodb.com>
2023-04-14 16:26:19 -0400

    use libmongocrypt 1.7.3 in go tset

--- test_with_godriver/run.sh
+++ test_with_godriver/run.sh
@@ -1,8 +1,9 @@
+# If needed: go clean -cache -testcache
 CERT_PATH=/Users/kevin.albertson/.secrets/hashicorp-vault/kmip_certs
 export KMIP_TLS_CERTIFICATE_KEY_FILE=$CERT_PATH/key_with_cert.pem
 export KMIP_TLS_CA_FILE=$CERT_PATH/ca.pem
-export PKG_CONFIG_PATH=~/code/c-bootstrap/install/libmongocrypt-1.7.1/lib/pkgconfig
-export DYLD_LIBRARY_PATH=~/code/c-bootstrap/install/libmongocrypt-1.7.1/lib/
+export PKG_CONFIG_PATH=~/code/c-bootstrap/install/libmongocrypt-1.7.3/lib/pkgconfig
+export DYLD_LIBRARY_PATH=~/code/c-bootstrap/install/libmongocrypt-1.7.3/lib/
 pushd test_with_godriver
 go run -tags cse .
 popd
